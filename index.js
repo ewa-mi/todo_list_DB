ad2f7ebc5d313bd168ea03fcca80725a5a3e59ea
ewami <efili1989@gmail.com>
2020-05-26 22:27:30 +0200

    config.json, port, Procfile, start script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const User = require("./models").user;
 const cors = require("cors");
 
